af43cbb82ac9c1c20a967e51ed0ad6239b5c39e2
joaomugart <joaomugart@gmail.com>
2018-07-05 21:52:55 -0300

    intial template

---
+++ src/App.vue
@@ -0,0 +1,83 @@
+<template>
+  <div id="app">
+
+    <v-app>
+      <v-navigation-drawer
+        v-model="drawer"
+        fixed
+        app
+      >
+        <v-list dense>
+          <v-list-tile @click="$router.push('/')">
+            <v-list-tile-action>
+              <v-icon>home</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>Home</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile @click="$router.push('/about')">
+            <v-list-tile-action>
+              <v-icon>info</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>About</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-toolbar color="indigo" dark fixed app>
+        <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+        <v-toolbar-title>TaskVue</v-toolbar-title>
+      </v-toolbar>
+
+      <v-content>
+        <v-container fluid fill-height>
+          <v-layout
+            justify-center
+            align-center
+          >
+            <v-flex text-xs-center>
+
+              <router-view/>
+
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-content>
+      <!-- FOOTER -->
+      <v-footer color="indigo" app>
+        <span class="white--text">&copy; 2018 - TaskVue</span>
+      </v-footer>
+    </v-app>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+    };
+  },
+};
+</script>
+
+
+<style lang="stylus">
+#app
+  font-family 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  color #2c3e50
+
+#nav
+  padding 30px
+  a
+    font-weight bold
+    color #2c3e50
+    &.router-link-exact-active
+      color #42b983
+</style>
